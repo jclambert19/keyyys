f307b935773c58dbcfa81528c559ac57cd036215
Jean-Christophe Lambert <iamjclambert@gmail.com>
2015-11-20 13:11:16 +0100

    navbar and front

--- app/models/bunch.rb
+++ app/models/bunch.rb
@@ -3,6 +3,6 @@ class Bunch < ActiveRecord::Base
   belongs_to :subscription
 
   validates :name, presence: true
-  validates :number_of_items, presence: true, numericality: { only_integer: true, greater_than: 1 }
+  validates :number_of_items, presence: true, numericality: { only_integer: true, greater_than: 0 }
   has_many :missions, dependent: :destroy
 end
